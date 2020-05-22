6bf69f1834ad26b2f8f581a49a4005affe1898c5
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-22 01:27:35 -0600

    Update students index spec with setup

--- spec/features/students/students_index_spec.rb
+++ spec/features/students/students_index_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Students Index Page", type: :feature do
-  describe "whe I visit the students index page" do
+  describe "when I visit the students index page" do
     before(:each) do
-      luna = Student.create!(name: "Luna Lovegood", age: 12, house: "Ravenclaw")
-      hermione = Student.create!(name: "Hermione Granger", age: 13, house: "Gryffindor")
+      @luna = Student.create!(name: "Luna Lovegood", age: 12, house: "Ravenclaw")
+      @hermione = Student.create!(name: "Hermione Granger", age: 13, house: "Gryffindor")
 
       visit "/students"
     end
 
     it "I see a list of students with personal information" do
-      expect(page).to have_content("Name: #{luna.name}")
-      expect(page).to have_content("Age: #{hermione.age}")
-      expect(page).to have_content("House: #{hermione.house}")
+      expect(page).to have_content("Name: #{@luna.name}")
+      expect(page).to have_content("Age: #{@hermione.age}")
+      expect(page).to have_content("House: #{@hermione.house}")
     end
 
     it "I see the average age of students" do
-      expect(page).to have_content(12.5)
+      expect(page).to have_content("Average student age: 12.5")
     end
   end
 end
